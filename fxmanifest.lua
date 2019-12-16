93578f6922fc0f9f70d8f7f2ee8c9ff4dc7d4670
Vespura <31419184+TomGrobbe@users.noreply.github.com>
2019-12-16 16:52:30 +0100

    Setup fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,1 +1,28 @@
--- soon
+name 'RedMenu'
+description 'Trainer / Menu created for the RedM community, useful for lots of things.'
+author 'Vespura'
+url 'https://github.com/tomgrobbe/redmenu/'
+version 'v0.0.1'
+
+-- Actual resource info setup stuff.
+fx_version 'adamant' -- I like this version
+games { 
+    'rdr3' -- Duh
+}
+
+-- Files for the client side.
+files {
+    'MenuAPI.dll',
+    'Newtonsoft.Json.dll',
+}
+
+-- Scripts
+client_scripts {
+    'RedMenuClient.net.dll'
+}
+server_scripts {
+    'RedMenuServer.net.dll'
+}
+
+-- Yes this is an early build, I know.
+rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
