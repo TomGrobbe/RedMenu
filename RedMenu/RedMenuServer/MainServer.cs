dd10b1c29418f326c60554e1ef0aa4d0e4beeef4
Vespura <31419184+TomGrobbe@users.noreply.github.com>
2019-12-16 15:51:39 +0100

    Create C# Project

---
+++ RedMenu/RedMenuServer/MainServer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CitizenFX.Core;
+using static CitizenFX.Core.Native.API;
+
+namespace RedMenuServer
+{
+    public class MainServer
+    {
+    }
+}
