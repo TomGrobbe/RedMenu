d1010926dddb4d82320603647bdfdd98f00482b8
Vespura <31419184+TomGrobbe@users.noreply.github.com>
2019-12-18 00:47:32 +0100

    Setup a TickFunctions class to manage all tick functions

---
+++ RedMenu/RedMenuClient/TickFunctions.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MenuAPI;
+using CitizenFX.Core;
+using static CitizenFX.Core.Native.API;
+
+namespace RedMenuClient
+{
+    class TickFunctions : BaseScript
+    {
+        public TickFunctions() { }
+
+        /// <summary>
+        /// Manages the radar toggle when holding down the select radar mode button.
+        /// Until more radar natives are discovered, this will have to do with only an on/off toggle.
+        /// </summary>
+        /// <returns></returns>
+        [Tick]
+        internal static async Task RadarToggleTick()
+        {
+            if (Util.IsControlPressed(Control.SelectRadarMode))
+            {
+                //UiPrompt promptY = new UiPrompt(new Control[1] { Control.ContextY }, "Compass");
+                //UiPrompt promptX = new UiPrompt(new Control[1] { Control.ContextX }, "Expanded");
+                UiPrompt promptA = new UiPrompt(new Control[1] { Control.ContextA }, "Regular");
+                UiPrompt promptB = new UiPrompt(new Control[1] { Control.ContextB }, "Off", "BRT2MountPrompt");
+                //promptY.Prepare();
+                //promptX.Prepare();
+                promptA.Prepare();
+                promptB.Prepare();
+                bool enabled = false;
+                while (Util.IsControlPressed(Control.SelectRadarMode))
+                {
+                    if (!enabled)
+                    {
+                        //promptY.SetEnabled(true, true);
+                        //promptX.SetEnabled(true, true);
+                        promptA.SetEnabled(true, true);
+                        promptB.SetEnabled(true, true);
+                        enabled = true;
+                    }
+
+                    if (Util.IsControlJustReleased(Control.ContextB))
+                    {
+                        DisplayRadar(false);
+                    }
+                    if (Util.IsControlJustReleased(Control.ContextA))
+                    {
+                        DisplayRadar(true);
+                    }
+                    //if (Util.IsControlJustReleased(Control.ContextX))
+                    //{
+                    //    DisplayRadar(true);
+                    //}
+                    //if (Util.IsControlJustReleased(Control.ContextY))
+                    //{
+                    //    DisplayRadar(true);
+                    //}
+
+                    await Delay(0);
+                }
+                //promptY.Dispose();
+                //promptX.Dispose();
+                promptA.Dispose();
+                promptB.Dispose();
+                while (Util.IsControlPressed(Control.SelectRadarMode))
+                {
+                    await Delay(0);
+                }
+            }
+            await Task.FromResult(0);
+        }
+    }
+}
