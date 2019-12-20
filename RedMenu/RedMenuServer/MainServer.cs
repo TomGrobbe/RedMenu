8c62ccd4015f492fa43401c95628b936849d4c5e
Vespura <31419184+TomGrobbe@users.noreply.github.com>
2019-12-20 21:31:57 +0100

    Check for valid a installation.

--- RedMenu/RedMenuServer/MainServer.cs
+++ RedMenu/RedMenuServer/MainServer.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,6 +13,16 @@ namespace RedMenuServer
     {
         public MainServer()
         {
+            if (!ConfigManager.EnablePermissions && !ConfigManager.IgnoreConfigWarning)
+            {
+                Debug.WriteLine("^3[WARNING] RedMenu is setup to ignore permissions! If this was not intended, please read the installation instructions. You can silence this warning by adding ^7setr ignore_config_warning \"true\" ^3to your server.cfg, above the ^7start RedMenu ^3line.^7");
+            }
+
+            if (GetCurrentResourceName() != "RedMenu")
+            {
+                Debug.WriteLine("^1[ERROR] RedMenu is not correctly installed. Please make sure that the folder is called RedMenu (case sensitive)! RedMenu will not function if it's incorrectly named.");
+                throw new Exception("Installation error: Invalid resource name.");
+            }
         }
     }
 }
