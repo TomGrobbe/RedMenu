e0852297bb732020e07e923dc0a6168284e501a7
Vespura <31419184+TomGrobbe@users.noreply.github.com>
2019-12-20 21:30:13 +0100

    Update initial menu setup.

--- RedMenu/RedMenuClient/MainClient.cs
+++ RedMenu/RedMenuClient/MainClient.cs
@@ -6,14 +6,56 @@ using System.Threading.Tasks;
 using MenuAPI;
 using CitizenFX.Core;
 using static CitizenFX.Core.Native.API;
+using RedMenuShared;
 
 namespace RedMenuClient
 {
     public class MainClient : BaseScript
     {
+
+        public static bool PermissionsSetupDone { get; internal set; } = false;
+
         public MainClient()
         {
-            menus.MainMenu.SetupMenu();
+            if (!ConfigManager.EnablePermissions && !ConfigManager.IgnoreConfigWarning)
+            {
+                Debug.WriteLine("^3[WARNING] RedMenu is setup to ignore permissions! If this was not intended, please read the installation instructions. You can silence this warning by adding ^7setr ignore_config_warning \"true\" ^3to your server.cfg, above the ^7start RedMenu ^3line.^7");
+            }
+
+            if (GetCurrentResourceName() == "RedMenu")
+            {
+                DelayedConstructor();
+            }
+            else
+            {
+                Debug.WriteLine("^1[ERROR] RedMenu is not correctly installed. Please make sure that the folder is called RedMenu (case sensitive)! RedMenu will not function if it's incorrectly named.");
+            }
+
+        }
+
+
+        /// <summary>
+        /// Delayed constructor waits for permissions to be setup before doing anything.
+        /// </summary>
+        private static async void DelayedConstructor()
+        {
+            while (!PermissionsSetupDone)
+            {
+                await Delay(0);
+            }
+
+            menus.MainMenu.GetMenu();
+
+            if (ConfigManager.UnlockFullMap)
+            {
+                SetMinimapHideFow(true);
+            }
+
+            // Needs more research.
+            //if (ConfigManager.EnableMaxStats)
+            //{
+
+            //}
         }
 
     }
--- RedMenu/RedMenuClient/menus/MainMenu.cs
+++ RedMenu/RedMenuClient/menus/MainMenu.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -17,24 +17,68 @@ namespace RedMenuClient.menus
         private static Menu mainMenu = new Menu("RedMenu", "Welcome to RedMenu!");
         private static bool setupDone = false;
 
-        public static void SetupMenu()
+        private static void SetupMenu()
         {
             if (setupDone) return;
             setupDone = true;
+            mainMenu.MenuTitle = GetPlayerName(PlayerId());
 
             MenuController.AddMenu(mainMenu);
-            MenuController.AddSubmenu(mainMenu, PlayerMenu.GetMenu());
-            PlayerMenu.SetupMenu();
 
-            MenuItem playerMenuBtn = new MenuItem("Player Menu", "All kinds of player related options.")
+            // Player Menu
+            if (PermissionsManager.IsAllowed(Permission.PMMenu))
+            {
+                MenuController.AddSubmenu(mainMenu, PlayerMenu.GetMenu());
+                MenuItem submenuBtn = new MenuItem("Player Menu", "All kinds of player related options.")
+                {
+                    RightIcon = MenuItem.Icon.ARROW_RIGHT
+                };
+
+                mainMenu.AddMenuItem(submenuBtn);
+                MenuController.BindMenuItem(mainMenu, PlayerMenu.GetMenu(), submenuBtn);
+            }
+
+            // Weapons Menu
+            if (PermissionsManager.IsAllowed(Permission.WMMenu))
+            {
+                MenuController.AddSubmenu(mainMenu, WeaponsMenu.GetMenu());
+                MenuItem submenuBtn = new MenuItem("Weapons Menu", "Weapon and ammo related options.")
+                {
+                    RightIcon = MenuItem.Icon.ARROW_RIGHT
+                };
+
+                mainMenu.AddMenuItem(submenuBtn);
+                MenuController.BindMenuItem(mainMenu, WeaponsMenu.GetMenu(), submenuBtn);
+            }
+
+
+            // Misc settings
+            MenuController.AddSubmenu(mainMenu, MiscSettingsMenu.GetMenu());
+            MenuItem miscBtn = new MenuItem("Misc Settings", "Miscellaneous settings and menu options.")
             {
                 RightIcon = MenuItem.Icon.ARROW_RIGHT
             };
 
-            mainMenu.AddMenuItem(playerMenuBtn);
-            MenuController.BindMenuItem(mainMenu, PlayerMenu.GetMenu(), playerMenuBtn);
+            mainMenu.AddMenuItem(miscBtn);
+            MenuController.BindMenuItem(mainMenu, MiscSettingsMenu.GetMenu(), miscBtn);
+
+
+            // Server Info
+            MenuController.AddSubmenu(mainMenu, ServerInfoMenu.GetMenu());
+            MenuItem serverBtn = new MenuItem("Server Info", "Information about this server.")
+            {
+                RightIcon = MenuItem.Icon.ARROW_RIGHT
+            };
+
+            mainMenu.AddMenuItem(serverBtn);
+            MenuController.BindMenuItem(mainMenu, ServerInfoMenu.GetMenu(), serverBtn);
+
         }
 
-        public static Menu GetMenu() { return mainMenu; }
+        public static Menu GetMenu()
+        {
+            SetupMenu();
+            return mainMenu;
+        }
     }
 }
