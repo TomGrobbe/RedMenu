b0ead11bdffa25b8b36e556d523bfe25b42e359c
Vespura <31419184+TomGrobbe@users.noreply.github.com>
2019-12-20 21:32:58 +0100

    New util function

--- RedMenu/RedMenuClient/util/Util.cs
+++ RedMenu/RedMenuClient/util/Util.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -122,6 +122,16 @@ namespace RedMenuClient
                 return true;
             }
             return false;
+        }
+
+        /// <summary>
+        /// Returns true if the control is enabled.
+        /// </summary>
+        /// <param name="control"></param>
+        /// <returns></returns>
+        public static bool IsControlEnabled(Control control)
+        {
+            return CitizenFX.Core.Native.Function.Call<bool>(CitizenFX.Core.Native.Hash.IS_CONTROL_ENABLED, 0, (uint)control);
         }
 
     }
