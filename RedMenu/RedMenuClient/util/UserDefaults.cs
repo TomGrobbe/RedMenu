2d8ec20c0f82c723f8b82d86909229b4bb012899
Vespura <31419184+TomGrobbe@users.noreply.github.com>
2019-12-20 21:32:48 +0100

    UserDefaults added.
    (Util class for saving/loading of user preferences)

---
+++ RedMenu/RedMenuClient/util/UserDefaults.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CitizenFX.Core;
+using static CitizenFX.Core.Native.API;
+using RedMenuShared;
+using CitizenFX.Core.Native;
+
+namespace RedMenuClient.util
+{
+    public static class UserDefaults
+    {
+
+        /// <summary>
+        /// Default: false
+        /// </summary>
+        public static bool PlayerGodMode
+        {
+            get
+            {
+                if (StorageManager.TryGet("PlayerGodMode", out bool val))
+                {
+                    return val;
+                }
+                PlayerGodMode = false;
+                return false;
+            }
+            set
+            {
+                StorageManager.Save("PlayerGodMode", value, true);
+            }
+        }
+
+        /// <summary>
+        /// Default: false
+        /// </summary>
+        public static bool PlayerInfiniteStamina
+        {
+            get
+            {
+                if (StorageManager.TryGet("PlayerInfiniteStamina", out bool val))
+                {
+                    return val;
+                }
+                PlayerInfiniteStamina = false;
+                return false;
+            }
+            set
+            {
+                StorageManager.Save("PlayerInfiniteStamina", value, true);
+            }
+        }
+
+        /// <summary>
+        /// Default: false
+        /// </summary>
+        public static bool PlayerInfiniteDeadEye
+        {
+            get
+            {
+                if (StorageManager.TryGet("PlayerInfiniteDeadEye", out bool val))
+                {
+                    return val;
+                }
+                PlayerInfiniteDeadEye = false;
+                return false;
+            }
+            set
+            {
+                StorageManager.Save("PlayerInfiniteDeadEye", value, true);
+            }
+        }
+
+        /// <summary>
+        /// Default: true
+        /// </summary>
+        public static bool MiscMinimapControls
+        {
+            get
+            {
+                if (StorageManager.TryGet("MiscMinimapControls", out bool val))
+                {
+                    return val;
+                }
+                MiscMinimapControls = true;
+                return true;
+            }
+            set
+            {
+                StorageManager.Save("MiscMinimapControls", value, true);
+            }
+        }
+
+    }
+}
