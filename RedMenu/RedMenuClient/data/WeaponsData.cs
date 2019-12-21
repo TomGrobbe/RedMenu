339945395737ac6aeb328bb2ae7b07805c25e136
Vespura <31419184+TomGrobbe@users.noreply.github.com>
2019-12-21 16:05:50 +0100

    Add some weapon data

--- RedMenu/RedMenuClient/data/WeaponsData.cs
+++ RedMenu/RedMenuClient/data/WeaponsData.cs
@@ -6,7 +6,59 @@ using System.Threading.Tasks;
 
 namespace RedMenuClient.data
 {
-    class WeaponsData
+    public static class WeaponsData
     {
+        public static List<string> WeaponHashes { get; } = new List<string>()
+        {
+            "WEAPON_MOONSHINEJUG",
+            "WEAPON_MELEE_LANTERN_ELECTRIC",
+            "WEAPON_MELEE_TORCH",
+            "WEAPON_MELEE_BROKEN_SWORD",
+            "WEAPON_FISHINGROD",
+            "WEAPON_MELEE_HATCHET",
+            "WEAPON_MELEE_CLEAVER",
+            "WEAPON_MELEE_ANCIENT_HATCHET",
+            "WEAPON_MELEE_HATCHET_VIKING",
+            "WEAPON_MELEE_HATCHET_HEWING",
+            "WEAPON_MELEE_HATCHET_DOUBLE_BIT",
+            "WEAPON_MELEE_HATCHET_DOUBLE_BIT_RUSTED",
+            "WEAPON_MELEE_HATCHET_HUNTER",
+            "WEAPON_MELEE_HATCHET_HUNTER_RUSTED",
+            "WEAPON_MELEE_KNIFE_JOHN",
+            "WEAPON_MELEE_KNIFE",
+            "WEAPON_MELEE_KNIFE_JAWBONE",
+            "WEAPON_THROWN_THROWING_KNIVES",
+            "WEAPON_MELEE_KNIFE_MINER",
+            "WEAPON_MELEE_KNIFE_CIVIL_WAR",
+            "WEAPON_MELEE_KNIFE_BEAR",
+            "WEAPON_MELEE_KNIFE_VAMPIRE",
+            "WEAPON_MELEE_MACHETE",
+            "WEAPON_THROWN_TOMAHAWK",
+            "WEAPON_THROWN_TOMAHAWK_ANCIENT",
+            //"WEAPON_PISTOL_M1899",
+            "WEAPON_PISTOL_MAUSER",
+            "WEAPON_PISTOL_SEMIAUTO",
+            "WEAPON_PISTOL_VOLCANIC",
+            "WEAPON_REPEATER_CARBINE",
+            "WEAPON_REPEATER_HENRY",
+            "WEAPON_RIFLE_VARMINT",
+            "WEAPON_REPEATER_WINCHESTER",
+            "WEAPON_REVOLVER_CATTLEMAN",
+            "WEAPON_REVOLVER_DOUBLEACTION",
+            "WEAPON_REVOLVER_LEMAT",
+            "WEAPON_REVOLVER_SCHOFIELD",
+            "WEAPON_RIFLE_BOLTACTION",
+            "WEAPON_SNIPERRIFLE_CARCANO",
+            "WEAPON_SNIPERRIFLE_ROLLINGBLOCK",
+            "WEAPON_RIFLE_SPRINGFIELD",
+            "WEAPON_SHOTGUN_DOUBLEBARREL",
+            "WEAPON_SHOTGUN_PUMP",
+            "WEAPON_SHOTGUN_REPEATING",
+            "WEAPON_SHOTGUN_SAWEDOFF",
+            "WEAPON_SHOTGUN_SEMIAUTO",
+            "WEAPON_BOW",
+            "WEAPON_THROWN_DYNAMITE",
+            "WEAPON_THROWN_MOLOTOV",
+        };
     }
 }
